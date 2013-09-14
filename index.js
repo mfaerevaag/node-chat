766b7fd4e3a8d059157a78e879feaedb1bab2dda
Markus Færevaag <mafaer@gmail.com>
2013-09-14 19:29:52 +0200

    Added welcome message

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var net = require('net');
 require('colors');
 
+
+var count = 0;
+
+
 var server = net.createServer(function (conn) {
-    // handle connection
-    console.log("    New connection!".grey);
-})
+    conn.write(
+        "\n > Welcome to " + "node-chat".blue + "!" +
+        "\n > " + count + " other users connected." +
+        "\n > Enter nick: "
+    );
+    count++;
+});
 
 server.listen(3000, function () {
     console.log("    Listening on port 3000".blue);
