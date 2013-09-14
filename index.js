38bb08ba44ac5cd98f8f4547ff0e9d163a221dc1
Markus Færevaag <mafaer@gmail.com>
2013-09-15 00:49:36 +0200

    Finished relay

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+/*
+ * node-chat
+ * Simple telnet chat client
+ * Based off Smashing Node.js
+ * @7SLEVIN
+ */
+
+
+/*
+ * Module dependencies
+ */
+
 var net = require('net');
 require('colors');
 
 
+/*
+ * Server
+ */
+
 var count = 0,
     users = {};
-
 
 var server = net.createServer(function (conn) {
     conn.setEncoding('utf8');
@@ -21,6 +36,7 @@ var server = net.createServer(function (conn) {
         data = data.replace("\r\n", "");
 
         if (!nickname) {
+            // new user
             if (users[data]) {
                 conn.write(" > Nickname taken. Try again: ".yellow);
                 return;
@@ -32,14 +48,27 @@ var server = net.createServer(function (conn) {
                     users[i].write(" > ".concat(nickname, " joined chat\n").grey);
                 }
             }
+        } else {
+            // chat message
+            for (var i in users) {
+                if (i != nickname) { // not send to self
+                    users[i].write(" > ".concat(nickname, ": ").blue + data + "\n");
+                }
+            }
         }
         console.log(data);
     });
 
     conn.on('close', function () {
         count--;
+        delete users[nickname];
     })
 });
+
+
+/*
+ * Listen
+ */
 
 server.listen(3000, function () {
     console.log("    Listening on port 3000".blue);
